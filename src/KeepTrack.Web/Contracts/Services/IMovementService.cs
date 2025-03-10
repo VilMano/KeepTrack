e5cd6d22d584bd34e80fb8e6503372bd5bcb4fef
VilMano <gsat.mano@gmail.com>
2025-03-10 17:25:00 +0000

    New features introduced on click names

--- src/KeepTrack.Web/Contracts/Services/IMovementService.cs
+++ src/KeepTrack.Web/Contracts/Services/IMovementService.cs
@@ -7,4 +7,8 @@ public interface IMovementService
     public Total GetTotal(int year, int month);
     
     public List<Movement> GetMovements(int year, int month);
+    
+    public List<Movement> GetMovementsByUser(int year, int month, string user);
+
+    public double GetYearAverage(int year);
 }
